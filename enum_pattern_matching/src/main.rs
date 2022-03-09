ca4a926726d817012aa0caafb66bf09c0ae5f67b
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-03-09 12:28:27 +0000

    Add enum pattern matching

---
+++ enum_pattern_matching/src/main.rs
@@ -0,0 +1,26 @@
+// Investigating basic enums and pattern matching
+// using chapter 6 of 'The Rust Programming Language'
+
+fn main() {
+    println!("Value of a penny: {}", value_in_cents(Coin::Penny));
+    println!("Value of a nickel: {}", value_in_cents(Coin::Nickel));
+    println!("Value of a dime: {}", value_in_cents(Coin::Dime));
+    println!("Value of a quarter: {}", value_in_cents(Coin::Quarter));
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+// Function to return a coin's value
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
