f0b19efc37d3c392b75013ef365bfc44579b7888
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-03-09 15:06:27 +0000

    Add restaurant

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,26 @@
+// Creating a basic library based on a restaurant model
+// using chapter 7 of 'The Rust Programming Language'
+
+pub use crate::front_of_house::serving;
+
+mod front_of_house;
+
+mod back_of_house;
+
+pub fn eat_breakfast() {
+    let mut meal = back_of_house::Breakfast::summer("rye");
+    meal.toast = String::from("wheat"); // Toast can be updated
+    println!("I'd like {} toast please", meal.toast);
+}
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist(); // Absolute path
+
+    front_of_house::hosting::seat_at_table(); // Relative path
+
+    serving::take_order(); // Using path brought into scope with use keyword
+    serving::serve_order();
+    serving::take_payment();
+}
+
+fn return_order() {} // Called by super
