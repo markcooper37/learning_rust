83c0a2005191abd1035f42399680536a58bd7ecd
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-03-09 00:21:10 +0000

    Add rectangles

---
+++ rectangles/src/main.rs
@@ -0,0 +1,47 @@
+// Creating a program to implement methods on rectangles
+// using chapter 5 of 'The Rust Programming Language'
+
+#[derive(Debug)]
+
+// Using a struct is more descriptive and clear,
+// and relates the height and width values to each other
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("rect1 is {:#?}", rect1);
+
+    println!(
+        "The area of rect1 is {} square pixels.",
+        rect1.area()
+    );
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
