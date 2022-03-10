a256d8d381f7d28384fe0269e861c4ba250c1aa5
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-03-10 11:17:19 +0000

    Add input check to guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -19,12 +19,17 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
         println!("You guessed: {}", guess);
+
+        if guess < 1 || guess > 100 {
+            println!("The secret number will be between 1 and 100.");
+            continue;
+        }
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
