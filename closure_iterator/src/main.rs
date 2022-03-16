53fa8cd6c16e70d608883f457a8b6348a5abd17c
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-03-16 17:41:56 +0000

    Add iterator

--- closure_iterator/src/main.rs
+++ closure_iterator/src/main.rs
@@ -4,6 +4,31 @@
 use std::thread;
 use std::time::Duration;
 
+fn main() {
+    // Closures
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(simulated_user_specified_value, simulated_random_number);
+
+    // Iterators
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
+
+    let v2: Vec<i32> = vec![1, 3, 5, 7];
+
+    let v3: Vec<_> = v2.iter().map(|x| x + 1).collect();
+
+    assert_eq!(v3, vec![2, 4, 6, 8]);
+}
+
+// CLOSURES
+
 // We use a cacher so that we can call a function only when necessary
 // and don't need to repeat the same operation twice
 struct Cacher<T>
@@ -62,9 +87,114 @@ fn generate_workout(intensity: u32, random_number: u32) {
     }
 }
 
-fn main() {
-    let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
+// ITERATORS
 
-    generate_workout(simulated_user_specified_value, simulated_random_number);
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+// This function uses into_iter to take ownership of the shoes vector
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+// Here, we create our own iterator
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+// Below, we test some iterator methods
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3, 10000];
+
+        let v1_iter = v1.iter();
+
+        let total: i32 = v1_iter.sum();
+
+        assert_eq!(total, 10006);
+    }
+
+    #[test]
+    fn iterator_demonstration() {
+        let v1 = vec![1, 2, 3, 10];
+
+        let mut v1_iter = v1.iter();
+
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), Some(&10));
+        assert_eq!(v1_iter.next(), None);
+    }
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                },
+            ]
+        );
+    }
+
+    #[test]
+    fn calling_next_directly() {
+        let mut counter = Counter::new();
+
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
 }
