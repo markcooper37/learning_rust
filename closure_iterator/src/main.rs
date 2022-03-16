42dc5881804c97fa38b57ae5d1cf75deba528da5
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-03-16 17:02:50 +0000

    Add closure

---
+++ closure_iterator/src/main.rs
@@ -0,0 +1,70 @@
+// Learning about closures and iterators
+// using chapter 13 of 'The Rust Programming Language'
+
+use std::thread;
+use std::time::Duration;
+
+// We use a cacher so that we can call a function only when necessary
+// and don't need to repeat the same operation twice
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        // The result of expensive_result.value(intensity) has already been calculated,
+        // so the following just gets the result from the cache
+        println!("Next, do {} situps!", expensive_result.value(intensity));
+    } else {
+        if random_number == 3 {
+            // We don't have to run expensive_result.value(intensity) in this case
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result.value(intensity)
+            );
+        }
+    }
+}
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(simulated_user_specified_value, simulated_random_number);
+}
