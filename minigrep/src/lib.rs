4563fd6a77eaed994924d48f564b3bac37363506
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-03-17 00:13:05 +0000

    Add iterators to project

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -28,13 +28,18 @@ pub struct Config {
 
 impl Config {
     // This function creates a new configuration
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
 
         // Environment variable option is included
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
@@ -46,15 +51,10 @@ impl Config {
 // This function performs a case sensitive search for a string,
 // returning the lines that contain it
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 // This function performs a case insensitive search for a string,
@@ -63,16 +63,10 @@ pub fn search_case_insensitive<'a>(
     query: &str,
     contents: &'a str,
 ) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
 
 // These tests check that the above search functions work as expected
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,8 +7,7 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         // Print to standard error
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
