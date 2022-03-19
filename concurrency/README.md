894d6be824ca2c5e73c86a327fe758ac3560a150
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-03-19 21:43:54 +0000

    Add concurrency

---
+++ concurrency/README.md
@@ -0,0 +1,2 @@
+# Concurrency
+Exploring concurrency using chapter 16 of 'The Rust Programming Language'.
---
+++ concurrency/src/main.rs
@@ -0,0 +1,80 @@
+use std::thread;
+use std::time::Duration;
+use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
+
+fn main() {
+    let v = vec![1, 2, 3];
+
+    // We use move to take ownership of v in the new thread
+    let handle = thread::spawn(move || {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread! Here's v: {:?}", i, v);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // Waits for thread to finish
+    handle.join().unwrap();
+
+    // Creating a channel
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+
+    // Creating a mutex
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+}
